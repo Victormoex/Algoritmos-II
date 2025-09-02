fb5ec78ac7914e9c8c42881bc1c5f0158c14ad5a
Victormoex <victorgms2019@gmail.com>
2025-09-02 17:47:24 -0400

    Add files via upload

---
+++ atvidade _04.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+//Formula de delta=D =a²-4.a.c
+// formula de baskara= -b +- raiz de D divido por 2.a
+ int baskara ( int a, int b, int c){
+    
+    
+    int delta=(b*b)-4*a*c;
+
+     if(delta<0){
+      printf("Sua função não tem raiz real")
+
+     }
+
+
+ }
+
+ int main(){
+    int a,b,C;
+
+    printf("Qual é o valor de A");
+    scanf("%d",&a);
+    printf("Qual é o valor de B");
+    scanf("%d",&b);
+    printf("Qual o valor de C");
+    scanf("%d",&c);
+   
+   delta(a,b,c)
+ 
+}
