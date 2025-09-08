12a0abc0789df56fc8f92d76af391691a1cb4620
Victormoex <victorgms2019@gmail.com>
2025-09-08 13:28:52 -0400

    Add files via upload

---
+++ exercicio2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int imprimir(char nome[30]){
+ 
+printf("%s",nome);
+}
+
+int main(){
+ char nome[30];
+
+    printf("Digite qualquer coisa para ser imprimido: ");
+    scanf("%s", &nome);
+    imprimir(nome);
+}
