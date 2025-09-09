9d9147ec1a387ca814dbb3177486cbdf595fb0d7
Victormoex <victorgms2019@gmail.com>
2025-09-09 17:03:47 -0400

    Add files via upload

---
+++ atividade_03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int texto(char nome [10]){
+
+    printf("%s",nome);
+    printf(" OK");
+    
+    return 0;
+}
+
+int main(){
+    char a[10];
+    scanf("%s",&a);
+    texto(a);
+
+}
