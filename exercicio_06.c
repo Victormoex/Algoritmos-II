d554f90728e00904a15c0a226cb57f6d2092284f
Victormoex <victorgms2019@gmail.com>
2025-09-12 17:30:11 -0400

    Add files via upload

---
+++ exercicio_06.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+
+int receber(int valor[10]){
+
+
+ for(int i=0 ; i<10; i++){
+
+        if(valor [i] >25) 
+        {
+
+            printf("\nSeu numero %d é maior que 25",valor[i]);
+
+        }else if(valor[i]  < 85) 
+        {
+
+            printf("\nSeu numero %d é menor que 85", valor[i]);
+        }
+        else{
+     
+
+        }
+
+
+    }
+
+
+}
+int main()
+{
+
+int valor[10];
+
+    printf("Digite 10 numeros inteiro: ");
+
+    for(int i=0; i<10; i++){
+        scanf("%d", &valor[i]);
+
+
+
+
+    }
+
+    receber(valor);
+
+
+
+}
