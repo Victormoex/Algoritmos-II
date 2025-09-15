3235d664996fd7f8d47af75560199c7ad2dc594f
Victormoex <victorgms2019@gmail.com>
2025-09-15 14:44:43 -0400

    Add files via upload

---
+++ atividade_03p.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+int numeros[10]={30,20,40,30,102,302,1,5,32,40};
+int *p1;
+
+p1 = &numeros[10];
+
+for(int i=0; i <10; i++){
+
+    printf("\n O endereço de numeros é %p e o numero é %d",p1,numeros[i]);
+    p1++;
+}
+
+
+}
