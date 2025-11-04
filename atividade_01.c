9307db6e7720c901d0172d4e5c4c6ba00b5410f7
Victormoex <victorgms2019@gmail.com>
2025-11-04 15:47:50 -0400

    Add program to display data type sizes

---
+++ atividade_01.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main()
+{
+
+ printf("Tamanho do byte do int: %d\n Tamanho do byte do char: %d\n  Tamanho do byte float: %d\n Tamanho do byte double: %d",sizeof(int),sizeof(char),sizeof(float),sizeof(double));
+
+
+}
