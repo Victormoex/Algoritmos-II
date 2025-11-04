c4d567d0e309a8dcee668666019419d63d3b832b
Victormoex <victorgms2019@gmail.com>
2025-11-04 16:43:35 -0400

    Add files via upload

---
+++ atividade2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdio.h>
+
+struct aluno_calculo
+{
+
+    int ra=5009;
+    int char[30]="rogerio";
+    float notas[3]={2.6, 5.7, 7.8};
+
+};
+
+int main()
+{
+    struct aluno_calculo=rogerio;
+
+    printf("O ra eh %d\n o nome eh %s\n "rogerio.ra,rogerio.char);
+
+        printf("as notas sao eh \n ")
+
+    for(int i=0;i<3;i++)
+    {
+        printf("%d",rogerio.notas[i]);
+
+    }
+
+
+
+}
