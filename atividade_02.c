010466da2b0fdac3dea2db53d8c0436cb7ece6bb
Victormoex <victorgms2019@gmail.com>
2025-11-06 00:45:51 -0400

    Add files via upload

---
+++ atividade_02.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+struct PlanoCartesiano{
+
+    int PontoX;
+    int PontoY;
+
+
+};
+
+typedef struct PlanoCartesiano PlanoCartesiano;
+
+int main(){
+ int origem[2]={0,0};
+ 
+ PlanoCartesiano carte;
+
+
+     printf("Digite um valor para X: ");
+     scanf("%d", &carte.PontoX);
+     
+     printf("Digite um valor para Y: ");
+     scanf("%d", &carte.PontoY);
+
+    origem[1]= carte.PontoX-0
+    origem[2]= carte.PontoY-0
+
+    
+
+}
