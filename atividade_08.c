7e34b520a640a6f95d1210a9c37320b27caab5af
Victormoex <victorgms2019@gmail.com>
2025-11-17 19:20:03 -0400

    Update atividade_08.c

--- atividade_08.c
+++ atividade_08.c
@@ -1,7 +1,23 @@
 #include<stdio.h>
 
 int main(){
+ int n;
+ int somatotal;
+    printf("Quantos numeros voce deseja digitar?: ");
+    scanf("%d", &n);
+     int numero[n];
+
+        for(int i=0;i<n;i++){
+            scanf("%d",&numero[i]);
+
+        }
 
 
-    
+     for(int i=0; i<n; i++){
+        somatotal+=numero[i];
+
+     }
+    printf("Sua soma e %d", somatotal);
 }
+
+
