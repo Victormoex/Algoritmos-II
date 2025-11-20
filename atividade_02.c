ab3010729e0fb4254dc821ce0b2c3ed75d891337
Victormoex <victorgms2019@gmail.com>
2025-11-20 14:47:11 -0400

    Refactor main function and fix variable assignments

--- atividade_02.c
+++ atividade_02.c
@@ -1,30 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
 
-struct PlanoCartesiano{
-
+struct PlanoCartesiano {
     int PontoX;
     int PontoY;
-
-
 };
 
 typedef struct PlanoCartesiano PlanoCartesiano;
 
-int main(){
- int origem[2]={0,0};
- 
- PlanoCartesiano carte;
+int main() {
+    int origem[2] = {0, 0};
+    PlanoCartesiano carte;
 
+    printf("Digite um valor para X: ");
+    scanf("%d", &carte.PontoX);
 
-     printf("Digite um valor para X: ");
-     scanf("%d", &carte.PontoX);
-     
-     printf("Digite um valor para Y: ");
-     scanf("%d", &carte.PontoY);
+    printf("Digite um valor para Y: ");
+    scanf("%d", &carte.PontoY);
 
-    origem[1]= carte.PontoX-0
-    origem[2]= carte.PontoY-0
+    origem[0] = carte.PontoX - 0;  
+    origem[1] = carte.PontoY - 0;  
 
-    
+    printf("\nDistância até a origem (separada por eixo):\n");
+    printf("Delta X = %d\n", origem[0]);
+    printf("Delta Y = %d\n", origem[1]);
 
+    return 0;
 }
