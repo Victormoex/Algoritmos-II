755c95bac1480cc071e90d83e039f0e12aeed09a
Victormoex <victorgms2019@gmail.com>
2025-11-26 22:36:51 -0400

    Refactor pointer usage and loop in ponteiro_04.c
    Fixed pointer initialization and adjusted loop structure.

--- ponteiro_04.c
+++ ponteiro_04.c
@@ -1,32 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
-
+int main() {
     float matriz[3][3];
     float *p;
 
-    p=matriz[0][0];
+    p = &matriz[0][0];  
+
     
-
     for(int i=0; i<3; i++){
-        for(int j=0; j<3;j++){
-        
+        for(int j=0; j<3; j++){
+            printf("Digite matriz[%d][%d]: ", i, j);
             scanf("%f", &matriz[i][j]);
-
         }
     }
 
-    for(int i=0; i<9;i++)
+    
+    for(int i=0; i<9; i++)
     {
-        printf("O endereco de linhas da matriz eh: %p\n",p );
-
-       p++;
-
-
+        printf("Endereco de matriz[%d] = %p\n", i, p);
+        p++;
     }
 
-
-
-
+    return 0;
 }
+
