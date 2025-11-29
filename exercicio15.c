243e5d2791e85962506ca3b8cb2491dd2e15450c
Victormoex <victorgms2019@gmail.com>
2025-11-29 02:16:04 -0400

    Update exercicio15.c

--- exercicio15.c
+++ exercicio15.c
@@ -1,6 +1,7 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-int* multiplicaVetores(int *A, int *B, int N)
+int* multiplicaVetores(int A[], int B[], int N) 
 {
     int *C = (int*) malloc(N * sizeof(int));
 
@@ -8,4 +9,32 @@ int* multiplicaVetores(int *A, int *B, int N)
         C[i] = A[i] * B[i];
 
     return C;
+}
+
+int main() {
+    int N;
+
+    printf("digite o tamanho do vetorzao ");
+    scanf("%d", &N);
+
+    int A[N], B[N];
+
+    printf("qual o valor de a");
+    for (int i = 0; i < N; i++)
+        scanf("%d", &A[i]);
+
+    printf("qual o valor de b");
+    for (int i = 0; i < N; i++)
+        scanf("%d", &B[i]);
+
+    int *C = multiplicaVetores(A, B, N);
+
+  
+    for (int i = 0; i < N; i++)
+        printf("%d ", C[i]);
+
+    
+
+    free(C);
+    return 0;
 }
