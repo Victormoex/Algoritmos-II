3ed001a916b2e8ac540f3eb2943cd0b38425db94
Victormoex <victorgms2019@gmail.com>
2025-11-29 02:13:09 -0400

    Refactor matrix creation for secondary diagonal

--- atividade_12.c
+++ atividade_12.c
@@ -1,15 +1,42 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-int** criaMatriz(int N)
+int** criaMatrizDiagonalSecundaria(int N) 
 {
-    int **M = (int**) malloc(N * sizeof(int*));
-    for (int i = 0; i < N; i++)
-        M[i] = (int*) malloc(N * sizeof(int));
+    if (N <= 0)
+        return NULL;
 
-    for (int i = 0; i < N; i++)
+    int **m = (int**) malloc(N * sizeof(int*));
+
+    for (int i = 0; i < N; i++) {
+        m[i] = (int*) malloc(N * sizeof(int));
         for (int j = 0; j < N; j++)
-            if (i + j == N - 1) M[i][j] = 1;
-            else M[i][j] = 0;
+            m[i][j] = (i + j == N - 1) ? 1 : 0;
+    }
 
-    return M;
+    return m;
+}
+
+int main() {
+    int N;
+
+    printf("digite o tamanho do vetrozao: ");
+    scanf("%d", &N);
+
+    int **mat = criaMatrizDiagonalSecundaria(N);
+
+   
+
+    printf("matriz:\n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+            printf("%d ", mat[i][j]);
+       
+    }
+
+    for (int i = 0; i < N; i++)
+        free(mat[i]);
+    free(mat);
+
+    return 0;
 }
