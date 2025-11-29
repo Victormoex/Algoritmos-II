fa70303d8cda54fc55499b9f0890695030e3d86c
Victormoex <victorgms2019@gmail.com>
2025-11-29 02:17:29 -0400

    Implement main function for matrix column sum
    Added main function to read matrix input and display column sums.

--- atividade_17.c
+++ atividade_17.c
@@ -1,16 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int* somaColunas(int **A, int N)
-{
+int* somaColunas(int **A, int N) {
     int *B = (int*) malloc(N * sizeof(int));
 
     for (int j = 0; j < N; j++) {
         B[j] = 0;
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++)
             B[j] += A[i][j];
-        }
     }
 
     return B;
+}
+
+int main() {
+    int N;
+    printf("digite um valor da matriz: ");
+    scanf("%d", &N);
+
+    int **A = (int**) malloc(N * sizeof(int*));
+
+    for (int i = 0; i < N; i++) {
+        A[i] = (int*) malloc(N * sizeof(int));
+        for (int j = 0; j < N; j++) {
+            printf("A[%d][%d]: ", i, j);
+            scanf("%d", &A[i][j]);
+        }
+    }
+
+    int *B = somaColunas(A, N);
+
+    
+    for (int j = 0; j < N; j++)
+        printf("%d ", B[j]);
+
+
+    for (int i = 0; i < N; i++)
+        free(A[i]);
+    free(A);
+    free(B);
+
+    return 0;
 }
